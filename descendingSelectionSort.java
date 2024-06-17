9c1e7830a857590df0d16997526d79b1f1380db4
Joaquin Descotte <157441085+Joaco273@users.noreply.github.com>
2024-06-17 16:18:16 -0400

    Update descendingSelectionSort.java
    Update, and debugging

--- descendingSelectionSort.java
+++ descendingSelectionSort.java
@@ -1,50 +1,52 @@
 import java.util.Scanner;
 public class descendingSelectionSort{
    public static void main(String[] args) {
-   Scanner keyboard = new Scanner (System.in);
-   System.out.print("Please write the amount of numbers that you want to input > ");   //entering length of the array
-   int numbers = keyboard.nextInt();
-   int array[] = new int [numbers];
-   
-   for (int x=0; x<array.length;x++){
-      System.out.print("Please input the value for score number "+(x+1)+" > ");
-      array[x] = keyboard.nextInt();         //filling array
-   }
-   
-   boolean rightOrder = isDescending(array);    //checking if the array is in the right descending position
-   int numberOfElements = numOfElements(array); //checking length of array
-   int temporary=0;
-   
-   
-   while(rightOrder == false){      //while order is not correct
-      int mI = minIndex(array, numberOfElements);  //finds index where minimum value is at
+      Scanner keyboard = new Scanner (System.in);
+      System.out.print("Please write the amount of numbers that you want to input > ");   //entering array.length
+      int numbers = keyboard.nextInt();
+      int array[] = new int [numbers];
       
-      
-      if(numberOfElements>0){
-         temporary = array[numberOfElements-1];
-         array[numberOfElements-1] = array[mI];
-         array[mI] = temporary;
-         //this is where the swaping takes place. First the temporary variable is equal to 
-         //the value at the indexed position at numberOfElements-1. Then the array at that
-         //position is equal to array at the place where the minimum index is located, and
-         // then to finalize the swapping, the array at mI is set equal to the temporary 
-         //variable completing the swapping of variables. 
-         //
-         //
-      }
-      rightOrder = isDescending(array);   //checking again if the array is in the correct order
-      if(numberOfElements>=0){
-         numberOfElements--;           //doing this, so that the sorted part of the array isn't 
-         //taken into consideration for the search of a minimum value
+      for (int x=0; x<array.length;x++){
+         System.out.print("Please input score n"+(x+1)+" > ");
+         array[x] = keyboard.nextInt();         //filling array
       }
       
-   }
-   for (int z=0; z<array.length; z++){
-      System.out.print(array[z] + ", ");  //printing array
-   }
-
-   keyboard.close();
-
+      boolean rightOrder = isDescending(array);    //checking if the array is in the right descending position
+      int numOfElements = array.length;
+      int temporary=0;
+      
+      
+      while(rightOrder == false){      //while order is not correct
+         int mI = minIndex(array, numOfElements);  //finds index where minimum value is at
+         
+         
+         if(numOfElements>0){
+            temporary = array[numOfElements-1];
+            array[numOfElements-1] = array[mI];
+            array[mI] = temporary;
+            //this is where the swaping takes place. First the temporary variable is equal to 
+            //the value at the indexed position at numOfElements-1. Then the array at that
+            //position is equal to array at the place where the minimum index is located, and
+            // then to finalize the swapping, the array at mI is set equal to the temporary 
+            //variable completing the swapping of variables. 
+            //
+            //
+         }
+         rightOrder = isDescending(array);   //checking again if the array is in the correct order
+         if(numOfElements>=0){
+            numOfElements--;           //doing this, so that the sorted part of the array isn't 
+            //taken into consideration for the search of a minimum value
+         }
+         
+      }
+      for (int z=0; z<array.length; z++){
+         if(z<(array.length-1)){
+            System.out.print(array[z] + ", ");
+         }else if (z==(array.length-1)){
+            System.out.print(array[z]);
+         }
+      }//end for loop
+      keyboard.close();
    }//end main
    
    public static int minIndex(int [] arr, int numOfEl){  //finding the index at which the minimum value is located
@@ -66,12 +68,4 @@ public class descendingSelectionSort{
     return true; // In descending order
    }//end isDescending
    
-   public static int numOfElements(int array[]){      //this determines the number of elements in the array
-      int num = 0;
-      for (int i=0;i<array.length;i++){
-         num++;
-      }//end for loop
-      
-      return num;
-   }//end numOfElements
-}
+}//end class
